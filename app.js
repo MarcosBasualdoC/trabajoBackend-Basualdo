4e9996ce01302faa57bd928fde27cb71030c0c19
Marcos Basualdo <marcosbasualdo96@hotmail.com>
2023-09-13 03:42:43 -0300

    SEGUNDA ENTREGA DEL TRABAJO

--- app.js
+++ app.js
@@ -9,36 +9,89 @@ const port = 8080;
 const productsRouter = require('./routes/products');
 const cartsRouter = require('./routes/carts');
 
-////
-app.engine('.hbs', exphbs({ extname: '.hbs' }));
+app.engine('.hbs', exphbs.engine({ extname: '.hbs', defaultLayout: '' }));
 app.set('view engine', '.hbs'); 
 app.set('views', path.join(__dirname, 'views'));
 
-////
 app.use(express.json());
 
-/////
 app.use('/api/products', productsRouter);
 app.use('/api/carts', cartsRouter);
 
-//////
-app.post('/agregar-producto', (req, res) => {
-    const { title, price } = req.body;
-    ////
+const fs = require('fs');
 
-    ///
-    io.emit('actualizarProductos', { title, price });
+app.get('/', (req, res) => {
+    fs.readFile('productos.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).send('Error al cargar los datos de productos');
+        }
 
-    res.redirect('/realtimeproducts'); 
+        const products = JSON.parse(data);
+
+        const dataToSend = {
+            products: products
+        };
+
+        res.render('home', dataToSend);
+    });
 });
 
+app.get('/realtimeproducts', (req, res) => {
+    fs.readFile('productos.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).send('Error al cargar los datos de productos');
+        }
+
+        const products = JSON.parse(data);
+
+        const dataToSend = {
+            products: products
+        };
+
+        res.render('realTimeProducts', dataToSend);
+    });
+});
+
+app.post('/realtimeproducts', (req, res) => {
+    const { title, price } = req.body;
+
+    fs.readFile('productos.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).send('Error al cargar los datos de productos');
+        }
+
+        const products = JSON.parse(data);
+
+        const newProduct = { title, price };
+        products.push(newProduct);
+
+        const updatedData = JSON.stringify(products, null, 2);
+
+        fs.writeFile('productos.json', updatedData, 'utf8', (err) => {
+            if (err) {
+                console.error(err);
+                return res.status(500).send('Error al agregar el nuevo producto');
+            }
+
+            io.emit('actualizarProductos', newProduct);
+
+            res.redirect('/realtimeproducts');
+        });
+    });
+});
 
 const server = http.createServer(app);
 const io = socketIo(server); 
 
 io.on('connection', (socket) => {
     console.log('Un cliente se ha conectado');
-    //////
+
+    socket.on('agregarProducto', (newProduct) => {
+        io.emit('actualizarProductos', newProduct);
+    });
 });
 
 server.listen(port, () => {
